0e3323eabf03fd900f20e2eddb8ce07f4fb60506
AlexSinMas23 <ba.mota@iesdoctorbalmis.com>
2021-10-05 21:27:30 +0200

    Agregar archivos de proyecto.

---
+++ CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuadrosTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyDownF1(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.F1)
+            {
+                this.TextoAyuda.Text = this.TextoAyuda.Text.Length == 0 ? "Ayuda." : "";
+            }
+        }
+        private void TextBox_KeyDownF2(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2)
+            {
+                int temo = 0;
+                if(int.Parse(TextoEdad.Text) == null)
+                {
+                    this.TextoAyuda.Text = this.TextoAyuda.Text.Length == 0 ? "No es un número." : "";
+                }
+            }
+        }
+    }
+}
